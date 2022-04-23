0a0d41cc46e79ab60eca3af34cb2482f621b49b2
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2022-04-24 00:21:45 +0300

    update-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
 
+startBtnRef.addEventListener('click', onStartBtnClick);
+stopBtnRef.addEventListener('click', onStopBtnClick);
+
+stopBtnRef.disabled = true;
+
+function onStartBtnClick() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtnRef.disabled = true;
+  stopBtnRef.disabled = false;
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  stopBtnRef.disabled = true;
+  startBtnRef.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
